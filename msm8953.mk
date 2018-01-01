8f36953059f31175da74ca07c868655bba83a995
Wei Wang <wvw@google.com>
2017-12-31 22:14:16 -0800

    power: Switch to libperfmgr for powerhint
    Test: boot and do powerhint
    Bug: 62041945
    Bug: 69270928
    Change-Id: I7de1d2bf377fb46162171a084fca3413b1067d3b

--- msm8953.mk
+++ msm8953.mk
@@ -314,19 +314,15 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
-# Perf
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.perf@2.0.vendor
-
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
+    android.hardware.power@1.2-service.onclite-libperfmgr \
     android.hardware.power@1.0.vendor \
     android.hardware.power@1.1.vendor \
     android.hardware.power@1.2.vendor
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+    $(LOCAL_PATH)/power/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Properties
 -include device/xiaomi/msm8953-common/prop.mk
