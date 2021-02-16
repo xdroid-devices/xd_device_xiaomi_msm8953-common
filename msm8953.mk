f475f577b37f93fd0ed7a6732385451c5a4d3c3f
Bruno Martins <bgcngm@gmail.com>
2021-02-16 13:44:18 +0000

    power: Migrate to Xiaomi power AIDL HAL
    Change-Id: I42170d51a517170b58d532addd9c38496e43457c

--- msm8953.mk
+++ msm8953.mk
@@ -316,7 +316,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.onclite-libperfmgr \
+    android.hardware.power-service.xiaomi-libperfmgr \
     android.hardware.power@1.0.vendor \
     android.hardware.power@1.1.vendor \
     android.hardware.power@1.2.vendor
