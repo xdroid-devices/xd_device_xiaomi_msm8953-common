26c62940c6f1fb1289579dcdc33545516bad910f
Arne Coucheron <arco68@gmail.com>
2021-08-14 15:38:09 +0530

    msm8953-common: Silence clang warnings during kernel compile
    clang-11: warning: argument unused during compilation:
    '-fuse-ld=lld' [-Wunused-command-line-argument]
    
    Change-Id: I66a629f3573e3fad8aa085ea115475f02d60a7ea
    Signed-off-by: rktdnt <rktdnt@proton.me>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -48,6 +48,7 @@ ifeq ($(TARGET_KERNEL_VERSION),4.9)
 BOARD_KERNEL_CMDLINE += androidboot.usbconfigfs=true
 TARGET_KERNEL_CLANG_COMPILE := true
 endif
+TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
 # ANT
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
