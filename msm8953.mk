7451a1cbb20c5b770159e721a05a9bf51de7dd6a
Quallenauge <Hamsi2k@freenet.de>
2021-09-15 19:40:28 +0100

    power: Rebrand xiaomi -> msm8953.
    Change-Id: I3cf39b3ce4d3ed7f35651b6268c3dcb67bd9e8db
    Signed-off-by: RayapKun <rayapkun@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -321,7 +321,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi-libperfmgr \
+    android.hardware.power-service.msm8953-libperfmgr \
     android.hardware.power@1.0.vendor \
     android.hardware.power@1.1.vendor \
     android.hardware.power@1.2.vendor
