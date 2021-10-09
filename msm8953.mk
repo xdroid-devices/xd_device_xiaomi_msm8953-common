dd14d7a5079d943a3179d0312a843f2fc221af6f
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-10-09 15:21:32 +0200

    msm8953-common: Build all extra VNDK versions
    * Basically make system a GSI, but this should avoid us the headache made by VNDK libs not being built based on proprietary interfaces
    
    Change-Id: I1ca876b5d2a82d41af76f4c327b89a181bb3d6bd
    Signed-off-by: RayapKun <rayapkun@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -433,6 +433,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/permissions/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/permissions/telephony_product_privapp-permissions-qti.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/telephony_product_privapp-permissions-qti.xml
 
+# VNDK
+PRODUCT_EXTRA_VNDK_VERSIONS := 28 29 30
+
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
