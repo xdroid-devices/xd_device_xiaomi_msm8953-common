afdc9fb78b03fbf9f4c9934e8f97cbd04b0792c4
Etienne Ruffieux <eruffieux@google.com>
2022-03-04 13:34:57 +0000

    msm8953-common: Removed audio A2dp from device.mk
    Bug: 220306859
    Test: atest
    Tag: #feature
    Change-Id: I300369d36399ffb558b98bafc2478b872d09faba
    Signed-off-by: rktdnt <rktdnt@proton.me>

--- msm8953.mk
+++ msm8953.mk
@@ -88,8 +88,9 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.audio.service \
-    android.hardware.soundtrigger@2.1-impl \
-    audio.a2dp.default \
+    android.hardware.soundtrigger@2.1-impl
+
+PRODUCT_PACKAGES += \
     audio.bluetooth.default \
     audio.primary.msm8953 \
     audio.r_submix.default \
